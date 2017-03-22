5d8e9173bc40da913dc081d29e18e170280c9bdc
abhinand-lingareddy <abhinand.lingareddy@gmail.com>
2017-03-21 23:41:25 -0400

    p2

--- src/avl.cpp
+++ src/avl.cpp
@@ -4,49 +4,32 @@ typedef struct node {
 	struct node *left;
 	struct node *right;
 	int key;
-	char *line;
 	int height;
-	int incre;
 } node;
 typedef struct text_t {
 	struct node*root;
-	int length;
 } text_t;
 text_t* create_text() {
 	text_t *temp = (text_t *) malloc(sizeof(text_t));
-	temp->length = 0;
 	temp->root = NULL;
 	return temp;
 }
-int length_text(text_t *txt) {
-	if (txt != NULL)
-		return txt->length;
-	else
-		return -1;
-}
-int current_increment;
+
 int get_index(node *n) {
-	return n->key + current_increment + n->incre;
+	return n->key;
 }
-void update_current_incre(node *n) {
-	current_increment = current_increment + n->incre;
-}
-node * search(text_t *txt, int index) {
-	current_increment = 0;
-	if (txt->length < index) {
-		return NULL;
-	}
-	node * temp = txt->root;
+
+bool search(node * temp, int index) {
 	while (temp != NULL) {
 		if (get_index(temp) == index) {
-			return temp;
+			return true;
 		} else if (get_index(temp) > index) {
 			temp = temp->left;
 		} else {
-			update_current_incre(temp);
 			temp = temp->right;
 		}
 	}
+	return false;
 
 }
 
@@ -72,7 +55,6 @@ int height(node *n) {
 node* rotate_left(node * root) {
 	node * temp = root;
 	root = root->right;
-	root->incre = root->incre + temp->incre;
 	node * child = root->left;
 	root->left = temp;
 	temp->right = child;
@@ -84,13 +66,10 @@ node* rotate_left(node * root) {
 node* rotate_right(node * root) {
 	node * temp = root;
 	root = root->left;
-	root->key = root->key + root->incre;
 	node *t = root->right;
 	while (t != NULL) {
-		t->incre = t->incre + root->incre;
 		t = t->left;
 	}
-	root->incre = 0;
 	node * child = root->right;
 	root->right = temp;
 	temp->left = child;
@@ -152,7 +131,7 @@ int check_balance_delete(node *n) {
 	return left_height - right_height;
 }
 
-void post_computation(node *temp, bool cdir) {
+void post_insert_computation(node *temp, bool cdir) {
 	if (state == 0) {
 		if (!check_balance(temp)) {
 			state = 1;
@@ -198,28 +177,22 @@ void post_computation_delete(node *temp, bool cdir) {
 	}
 	height(temp);
 }
-void update_key(node *n) {
-	n->key = n->key - current_increment;
-}
+
 void insert(node* temp, int index, node *new_node) {
 	if (get_index(temp) < index) {
-		update_current_incre(temp);
 		if (temp->right != NULL) {
 			insert(temp->right, index, new_node);
-			post_computation(temp, true);
+			post_insert_computation(temp, true);
 		} else {
-			update_key(new_node);
 			temp->right = new_node; //balance
 			temp->height = 1;
 			dir = true;
 		}
 	} else {
-		temp->incre = temp->incre + 1;
 		if (temp->left != NULL) {
 			insert(temp->left, index, new_node);
-			post_computation(temp, false);
+			post_insert_computation(temp, false);
 		} else {
-			update_key(new_node);
 			temp->left = new_node; //balance
 			temp->height = 1;
 			dir = false;
@@ -234,10 +207,10 @@ void delete_inorder(node *n, node *c) {
 		delete_inorder(n, c->left);
 		post_computation_delete(c, false);
 	} else {
-		n->line = c->left->line;
+		//copy data
+		n->key=c->left->key;
 		c->left = delete_current_node(c->left);
-		n->key=n->key+1;
-		n->incre=n->incre-1;
+		//n->key=n->key+1;
 		//free c
 	}
 }
@@ -245,67 +218,60 @@ void delete_inorder(node *n, node *c) {
 node* delete_current_node(node *n) {
 	if (n->left == NULL) {
 		if (n->right == NULL) {
-			// free n
+			free(n);
 			return NULL;
 		} else {
-			//free n
-			n->right->incre = n->right->incre + n->incre - 1;
-			return n->right;
+			node * t = n->right;
+			free(n);
+			return t;
 		}
 	} else if (n->right == NULL) {
-		//free n
-		return n->left;
+		node *t = n->left;
+		free(n);
+		return t;
 	} else {
 		if (n->right->left != NULL) {
 			delete_inorder(n, n->right);
 		} else {
-			n->line = n->right->line;
+			//copy data of deltion node
+			n->key=n->right->key;
 			n->right = delete_current_node(n->right);
 		}
 		post_computation_delete(n, true);
 		return n;
 	}
 }
-char* delete_node(node* n, int index) {
-	char *temp = NULL;
+void search_delete_node(node* n, int index) {
 	if (get_index(n) < index) {
 		if (n->right != NULL) {
-			update_current_incre(n);
 			if (get_index(n->right) != index) {
-				temp = delete_node(n->right, index);
+				search_delete_node(n->right, index);
 				post_computation_delete(n, true);
 			} else {
-				temp = n->right->line;
 				n->right = delete_current_node(n->right);
 				post_computation_delete(n, true);
 			}
 		}
 	} else {
-		n->incre = n->incre - 1;
 		if (n->left != NULL) {
 			if (get_index(n->left) != index) {
-				temp = delete_node(n->left, index);
+				search_delete_node(n->left, index);
 				post_computation_delete(n, false);
 			} else {
-				temp = n->left->line;
 				n->left = delete_current_node(n->left);
 				post_computation_delete(n, false);
 			}
 		}
 	}
-	return temp;
 
 }
-node * get_node(char *new_line, int key, int *length) {
+node * get_node(int key) {
 	node *temp = (node *) malloc(sizeof(node));
 	if (temp != NULL) {
-		temp->line = new_line;
 		temp->left = NULL;
 		temp->right = NULL;
 		temp->key = key;
-		temp->incre = 0;
 		temp->height = 0;
-		*length = *length + 1;
 	}
 	return temp;
 }
@@ -313,81 +279,70 @@ node * get_node(char *new_line, int key, int *length) {
 void intialize_root(text_t *txt, node * new_node) {
 	txt->root = new_node;
 }
-void insert_line(text_t *txt, int index, char * new_line) {
+void insert_line(text_t *txt, int index) {
 	state = 0;
-	if (txt->length + 1 < index) {
-		index = txt->length + 1;
-	}
-	current_increment = 0;
-	node * temp = txt->root;
-	node *new_node = get_node(new_line, index, &(txt->length));
+	node * troot = txt->root;
+	node *new_node = get_node(index);
 	if (new_node != NULL) {
-		if (temp != NULL) {
-			insert(temp, index, new_node);
+		if (troot != NULL) {
+			insert(troot, index, new_node);
 			if (state == 1) {
-				txt->root = balance(temp, dir, pdir);
+				txt->root = balance(troot, dir, pdir);
 			}
 		} else
 			intialize_root(txt, new_node);
 	}
 
 }
-char * get_line(text_t *txt, int index) {
-	node *temp = search(txt, index);
-	if (temp != NULL) {
-		return temp->line;
-	}
-	return NULL;
-}
 
-char * set_line(text_t *txt, int index, char * new_line) {
-	node *temp = search(txt, index);
-	char *old_line = NULL;
-	if (temp != NULL) {
-		old_line = temp->line;
-		temp->line = new_line;
-	}
-	return old_line;
-}
-void append_line(text_t *txt, char * new_line) {
-	insert_line(txt, txt->length + 1, new_line);
-}
-
-char * delete_line(text_t *txt, int index) {
+void delete_line(text_t *txt, int index) {
 	state = 0;
 	node* n = txt->root;
-	if (index > txt->length) {
-		return NULL;
-	}
-	current_increment = 0;
-	txt->length = txt->length - 1;
 	if (n != NULL) {
-		char *t;
 		if (get_index(n) == index) {
-			t = n->line;
 			txt->root = delete_current_node(n);
 		} else {
-			t = delete_node(n, index);
+			search_delete_node(n, index);
 		}
 		if (state == 1) {
 			txt->root = balance(txt->root, dir, pdir);
 		}
-		return t;
+	}
+
+}
+int count=0;
+int inorder(node * t) {
+	if (t != NULL) {
+		count++;
+		int hleft = inorder(t->left);
+		int hright = inorder(t->right);
+		int h;
+		if (hleft > hright) {
+			h = hleft + 1;
+		} else {
+			h = hright + 1;
+		}
+		if (t->height != h) {
+			printf("issue at %d \n", t->key);
+		}
+		int balanced = hleft - hright;
+		if (balanced < -1 || balanced > 1) {
+			printf("unbalanced at %d\n", t->key);
+		}
+		return h;
 	} else {
-		return NULL;
+		return -1;
 	}
-
 }
 
-void inorder(node *n) {
-	if (n->left != NULL)
-		inorder(n->left);
-	printf("%d %s\n", get_index(n), n->line);
-	if (n->right != NULL) {
-		update_current_incre(n);
-		inorder(n->right);
-		current_increment = current_increment - n->incre;
+int main() {
+	text_t* t = create_text();
+	for (int i = 0; i < 4000000; i++) {
+		insert_line(t, i);
 	}
 
-}
 
+	printf("%d\n", t->root->height);
+	inorder(t->root);
+	printf("done count %d \n",count);
+}
